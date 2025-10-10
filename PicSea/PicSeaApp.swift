8366c69a15f522df3421d501756fefbab0e58ab3
Sydney <sydlyn626@gmail.com>
2025-10-09 21:28:28 -0700

    new switft proj

--- PicSea/PicSeaApp.swift
+++ PicSea/PicSeaApp.swift
@@ -5,13 +5,37 @@
 //  Created by Sydney Lynch on 10/9/25.
 //
 
-import SwiftUI
+import Vision
+struct ImageFile {
+  var url: URL
+  var observations: [String: VNConfidence] = [:]
+}
+
+func classifyImage(url: URL) async throws -> ImageFile {
+  var image = ImageFile(url: url)
+  let request = ClassifyImageRequest()
+
+  let results = try await request.perform(on: url)
+    .filter { $0.hasMinimumPrecision(0.1, forRecall: 0.8) }
+  for classification in results {
+    image.observations[classification.identifier] = classification.confidence
+  }
+  return image
+}
 
 @main
-struct PicSeaApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+struct ClassificationTestApp {
+    static func main() async {
+        do {
+            guard let url = Bundle.main.url(forResource: "bobby", withExtension: "png") else {
+                print("Image not found in app bundle.")
+                return
+            }
+            let file = try await classifyImage(url: url)
+            print("Classification succeeded:")
+            print(file)
+        } catch {
+            print("Classification failed:", error)
         }
     }
 }
