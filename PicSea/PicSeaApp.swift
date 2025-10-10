a5b7d80a934f54f7b7d7ce155fb79997af41e5b3
Sydney <sydlyn626@gmail.com>
2025-10-09 21:09:05 -0700

    Initial Commit

---
+++ PicSea/PicSeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicSeaApp.swift
+//  PicSea
+//
+//  Created by Sydney Lynch on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PicSeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PicSeaTests/PicSeaTests.swift
@@ -0,0 +1,16 @@
+//
+//  PicSeaTests.swift
+//  PicSeaTests
+//
+//  Created by Sydney Lynch on 10/9/25.
+//
+
+import Testing
+
+struct PicSeaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
