2b88de3b9c51f0108fc4cd6e4b34d656bd9f4ec6
Sydney <sydlyn626@gmail.com>
2025-10-15 19:39:05 -0700

    combine photo library view with bobby classifer

--- PicSea/PicSeaApp.swift
+++ PicSea/PicSeaApp.swift
@@ -4,56 +4,52 @@
 //
 
 import SwiftUI
+import Vision
 
 @main
 struct PicSeaApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .task {
+                    await classifyBobby()
+                }
         }
     }
+    
+    func classifyBobby() async {
+        do {
+            // get the image of bobby from the project folder
+            guard let url = Bundle.main.url(forResource: "bobby", withExtension: "png") else {
+                print("Image not found in app bundle.")
+                return
+            }
+            // run the classifier
+            let result = try await classifyImage(url: url)
+            print("Classification succeeded:")
+            print(result.observations)
+        } catch {
+            print("Classification failed:", error)
+        }
+    }
+    
+    struct ImageFile {
+        var url: URL
+        var observations: [String: VNConfidence] = [:]
+    }
+    
+    func classifyImage(url: URL) async throws -> ImageFile {
+        var image = ImageFile(url: url)
+        let request = ClassifyImageRequest()
+
+        let results = try await request.perform(on: url)          // THE api call
+            .filter { $0.hasMinimumPrecision(0.1, forRecall: 0.8) }
+
+        // add each result to the output struct
+        for classification in results {
+            image.observations[classification.identifier] = classification.confidence
+        }
+        
+        return image
+    }
 }
-//
-//
-//import Vision
-//
-//// struct to hold image url and output classifiers
-//struct ImageFile {
-//    var url: URL
-//    var observations: [String: VNConfidence] = [:]
-//}
-//
-//// function that takes in an image url and calls the Apple classifier API
-//func classifyImage(url: URL) async throws -> ImageFile {
-//    var image = ImageFile(url: url)
-//    let request = ClassifyImageRequest()
-//
-//    let results = try await request.perform(on: url)          // THE api call
-//        .filter { $0.hasMinimumPrecision(0.1, forRecall: 0.8) }
-//
-//    // add each result to the output struct
-//    for classification in results {
-//        image.observations[classification.identifier] = classification.confidence
-//    }
-//    
-//    return image
-//}
-//
-//@main
-//struct ClassificationTestApp {
-//    static func main() async {
-//        do {
-//            // get the image of bobby from the project folder
-//            guard let url = Bundle.main.url(forResource: "bobby", withExtension: "png") else {
-//                print("Image not found in app bundle.")
-//                return
-//            }
-//            // run the classifier
-//            let result = try await classifyImage(url: url)
-//            print("Classification succeeded:")
-//            print(result.observations)
-//        } catch {
-//            print("Classification failed:", error)
-//        }
-//    }
-//}
--- PicSea/Services/MLClassifier.swift
+++ PicSea/Services/MLClassifier.swift
@@ -6,25 +6,25 @@
 import Foundation
 import Vision
 
-struct ImageFile {
-    var url: URL
-    var observations: [String: VNConfidence] = [:]
-}
-
-struct MLClassifier {
-    static func classifyImage(url: URL) async throws -> ImageFile {
-        var image = ImageFile(url: url)
-        let request = ClassifyImageRequest()
-
-        let results = try await request.perform(on: url)
-            .filter { $0.hasMinimumPrecision(0.1, forRecall: 0.8) }
-
-        for classification in results {
-            image.observations[classification.identifier] = classification.confidence
-        }
-
-        return image
-    }
-}
-
-
+//struct ImageFile {
+//    var url: URL
+//    var observations: [String: VNConfidence] = [:]
+//}
+//
+//struct MLClassifier {
+//    static func classifyImage(url: URL) async throws -> ImageFile {
+//        var image = ImageFile(url: url)
+//        let request = ClassifyImageRequest()
+//
+//        let results = try await request.perform(on: url)
+//            .filter { $0.hasMinimumPrecision(0.1, forRecall: 0.8) }
+//
+//        for classification in results {
+//            image.observations[classification.identifier] = classification.confidence
+//        }
+//
+//        return image
+//    }
+//}
+//
+//
